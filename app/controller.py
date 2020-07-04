c2c6cf03b860901d3a7de83a02144dea05317d08
BMilliet <brunomilliet89@gmail.com>
2020-07-04 12:27:00 -0300

    parameters for response

--- app/controller.py
+++ app/controller.py
@@ -1,36 +1,30 @@
 import time
-from flask import render_template
+from flask import render_template, Response, json
 from . import jsonLoader, decodables
 
 
-services = []
-
-control = jsonLoader.loadJson('./control/control.json')
-for service in control['services']:
-    services.append(decodables.Service.from_dict(service))
-
-
-def get_response_for_route(path, delay=0):
+def get_response_for_route(path):
+    services = _get_services()
     if path == 'index':
         return _render_index()
-    time.sleep(delay)
-    return _get_response(path)
+    return _get_response(path, services)
 
 
-def _get_response(path):
+def _get_response(path, services):
     for s in services:
         for p in s.get_paths():
             if _has_route_with_parameter(path, p):
-                json_path = s.get_response_for_path(p)
-                return jsonLoader.loadJson(json_path)
-    return _get_response_for_path(path)
+                response = s.get_response_for_path(p)
+                return _build_reponse(response)
+
+    return _get_response_for_path(path, services)
 
 
-def _get_response_for_path(path):
+def _get_response_for_path(path, services):
     for s in services:
         if path in s.get_paths():
-            json_path = s.get_response_for_path(path)
-            return jsonLoader.loadJson(json_path)
+            response = s.get_response_for_path(path)
+            return _build_reponse(response)
     return 'path not found'
 
 
@@ -42,4 +36,21 @@ def _has_route_with_parameter(path1, path2):
 
 
 def _render_index():
-    return render_template('index.html', services=services)
+    return render_template('index.html', services=_get_services())
+
+
+def _get_services():
+    services = []
+
+    control = jsonLoader.loadJson('./control/control.json')
+    for service in control['services']:
+        services.append(decodables.Service.from_dict(service))
+    return services
+
+
+def _build_reponse(response_tuple):
+    time.sleep(response_tuple[2])
+    body = jsonLoader.loadJson(response_tuple[0])
+    return Response(response=json.dumps(body),
+                    status=response_tuple[1],
+                    mimetype='application/json')
